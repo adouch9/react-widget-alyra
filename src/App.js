6ab8704f7885fdbfdcd073008c00fc748985950d
adouch9 <mr.ouldmoussa@gmail.com>
2021-05-03 22:41:33 +0200

    projet

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import './style/App.css';
+import Header from "./compenents/Header"
+import Footer from "./compenents/Footer"
+
+function App() {
+  return (
+    <div className="App">
+     <Header />
+     <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/compenents/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Header = () => {
+
+    return (
+        
+<header className= "bg-danger text-center text-white p-5">
+    <h1 className= "display-1">Quoi de neuf, Google Fonts™ ?</h1>
+    <p className= "h4">Le plus récentes, les plus <i>trendy</i> et les plus populaires polices Google.</p>
+</header>
+    )
+}
+
+export default Header 
